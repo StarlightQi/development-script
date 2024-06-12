010a8dac9257920cbe2684b3da1a09083e717e3e
Your <you@example.com>
2024-06-12 19:48:41 +0800

    优化JSON DB 以及Tool代码

--- python-script/auto-deployment/tool/MavenTool.py
+++ python-script/auto-deployment/tool/MavenTool.py
@@ -1,6 +1,7 @@
 import os
 
 from Tool import Tool
+import xml.etree.ElementTree as ET
 
 
 class MavenTool(Tool):
@@ -32,15 +33,70 @@ class MavenTool(Tool):
         super().log_add(log)
         print(f"maven {log}")
 
-    def install(self):
-        return self.run_maven_mvn_log("install")
+    def install(self, path=None):
+        """
+        指定开发目录
+        :param path:
+        :return:
+        """
+        if path is None or os.path.exists(path):
+            return self.run_maven_mvn_log(f"install -Dmaven.repo.local={path}")
+        else:
+            return self.run_maven_mvn_log("install")
         # return self.run_maven_mvn_log("install", lambda log: print(f"test :{log}"))
+
+    def get_version_from_pom(self, pom_file):
+        # 解析 XML 文件
+        tree = ET.parse(pom_file)
+        root = tree.getroot()
+
+        # XML 命名空间
+        namespace = {'maven': 'http://maven.apache.org/POM/4.0.0'}
+
+        # 查找 <version> 元素
+        version_element = root.find('maven:version', namespace)
+        if version_element is None:
+            # 如果在根层次找不到 <version>，可能是在 <parent> 标签里
+            parent_version = root.find('maven:parent/maven:version', namespace)
+            if parent_version is not None:
+                return parent_version.text
+            return None
+        else:
+            return version_element.text
 
     def clean(self):
         return self.run_maven_mvn_log("clear")
 
     def compile(self):
         return self.run_maven_mvn_log("compile")
+
+    def find_maven_settings(self, custom_path=None):
+        # 如果提供了自定义路径，则使用该路径
+        if custom_path:
+            return custom_path
+        # 尝试在用户的 .m2 目录下查找 settings.xml
+        home_settings = os.path.expanduser('~/.m2/settings.xml')
+        if os.path.exists(home_settings):
+            return home_settings
+        if self.maven_home:
+            maven_settings = os.path.join(self.maven_home, 'conf', 'settings.xml')
+            if os.path.exists(maven_settings):
+                return maven_settings
+        return None
+
+    def get_local_repository_path(self, settings_path):
+        settings_file_path = settings_path if settings_path else self.find_maven_settings()  # You can also pass a path here
+        try:
+            if settings_file_path:
+                tree = ET.parse(settings_file_path)
+                root = tree.getroot()
+                namespace = {'maven': 'http://maven.apache.org/POM/4.0.0'}
+                local_repository = root.find('maven:localRepository', namespace)
+                if local_repository is not None:
+                    return local_repository.text
+            return None
+        except Exception as e:
+            return None
 
 
 if __name__ == '__main__':
